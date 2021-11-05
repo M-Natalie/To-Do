13157a35168b7c7b4b7d1e2c79c072e263f7bcc0
M-Natalie <93541716+M-Natalie@users.noreply.github.com>
2021-11-05 11:07:48 +0600

    admin panal, data base

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TODOLIST, ToMeet
+
+
+admin.site.register(TODOLIST)
+admin.site.register(ToMeet)
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class TODOLIST(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+class ToMeet(models.Model):
+    person = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    data_of_meeting = models.DateField()
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
 # Create your models here.
